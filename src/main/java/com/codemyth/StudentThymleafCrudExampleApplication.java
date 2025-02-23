1d1b9e90657b6aec935aacef263f36a652826359
ganesh-legend <ganeshpawar06969@gmail.com>
2025-02-23 18:42:49 +0530

    Code Added

---
+++ src/main/java/com/codemyth/StudentThymleafCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.codemyth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentThymleafCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentThymleafCrudExampleApplication.class, args);
+	}
+
+}
